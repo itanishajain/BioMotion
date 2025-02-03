bcad4d7032613da413eace295204f01c93583306
Tanisha Jain <itanishajain@gmail.com>
2025-02-03 16:38:13 +0530

    added venv folder in git ignore

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c68d83886a60f0ca048fc36b2ac09ae994103eed22e6379bfb77b5c60fdde33a
-size 1953
+oid sha256:bd241d230f1d19971c9ed22d07610e59cf14b3f0ed6d30b6c2a11dbfae3c04f1
+size 1976
